9421530376bb11d4c7c62394fd71ac295a2574df
Vidushi Bhadola <32338197+Vidlink@users.noreply.github.com>
2018-09-28 22:10:38 +0530

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// THIS IS A PROGRAM TO CREATE A BINARY SEARCH TREE BY INSERTING NODES AND DISPLAYING AN IN-ORDER TRAVERSAL OF THE TREE
 package binarysearchtree;
 
 /**
